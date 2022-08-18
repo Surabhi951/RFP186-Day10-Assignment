3b87d421d4b99d0ae2fd156e0e52524c198ba306
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-18 17:35:20 +0530

    Added multiple persons

--- src/com/bridgelabz/addressBook/ContactPerson.java
+++ src/com/bridgelabz/addressBook/ContactPerson.java
@@ -10,7 +10,15 @@ public class ContactPerson {
     private long phoneNumber;
     private String email;
 
-    public ContactPerson() {
+    public ContactPerson(String firstName, String lastName, String address, String city, String state, int zipCode, long phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -21,29 +29,39 @@ public class ContactPerson {
         this.firstName = firstName;
     }
 
+
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
 
     public void setAddress(String address) {
         this.address = address;
     }
 
+
+
     public void setCity(String city) {
         this.city = city;
     }
+
 
     public void setState(String state) {
         this.state = state;
     }
 
+
+
     public void setZipCode(int zipCode) {
         this.zipCode = zipCode;
     }
 
+
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
 
     public void setEmail(String email) {
         this.email = email;
@@ -51,13 +69,13 @@ public class ContactPerson {
 
     @Override
     public String toString() {
-        return "ContactPerson{" +
+        return "Contact{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", address='" + address + '\'' +
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
-                ", zipCode=" + zipCode +
+                ", zip=" + zipCode +
                 ", phoneNumber=" + phoneNumber +
                 ", email='" + email + '\'' +
                 '}';
